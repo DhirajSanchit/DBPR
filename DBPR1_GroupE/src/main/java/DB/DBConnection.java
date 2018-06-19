90b58cbdf2cc36cb80b476afa72772e13d70af69
Amit Dhiraj Sanchit <Dhirajsanchit@gmail.com>
2018-06-20 00:29:45 +0200

    Setup DB connectie
    Zelf de DB connecties invullen.

--- DBPR1_GroupE/src/main/java/DB/DBConnection.java
+++ DBPR1_GroupE/src/main/java/DB/DBConnection.java
@@ -8,15 +8,22 @@ public class DBConnection {
     /**
      * @param args the command line arguments
      */
+
+
+        static String username = "appie3";
+        static String password = "admin1234567890";
+
     public static void main(String[] args) {
-        String url = "jdbc:sqlserver://localhost:1433;databaseName=AuditBlackBox;user=appie2;password=1234;";
+        String url = "jdbc:sqlserver://localhost:1433;databaseName=AuditBlackBox";
         Connection con = null;
 
         try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            con = DriverManager.getConnection(url);
+                con = DriverManager.getConnection(url,
+                        username,
+                        password);
 
-            if (con != null) {
+                if (con != null) {
                 System.out.println("Connected to db succesfully!");
             }
         } catch (ClassNotFoundException e) {
